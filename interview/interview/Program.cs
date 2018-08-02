f6c798e6b0355398dc3e86d69b65acc800ade199
QianXi <2470460089@qq.com>
2018-08-02 10:51:07 +0800

    八大排序算法(EightSortAlgorithm.cs脚本)

---
+++ interview/interview/Program.cs
@@ -0,0 +1,17 @@
+﻿using EightSortAlgorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 八大排序算法
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 6, 5, 9, 16, 45, 35, 8, 13, 25, 60, 22, 31 };
+        arr.ShellSort();
+    }
+}
